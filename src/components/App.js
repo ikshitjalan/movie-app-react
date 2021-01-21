e952be1de8a3dce8fca6ba64461f475e9b1cd23d
ikshitjalan <jalanikshit1@gmail.com>
2021-01-22 02:55:06 +0530

    store added

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,31 @@
 import React from 'react';
-import { data } from '../data';
 import Navbar from './Navbar';
 import MovieCard from './MovieCard';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <div className="main">
-        <div className="tabs">
-          <div className="tab">Movies</div>
-          <div className="tab">Favourites</div>
-        </div>
-        <div className="list">
-          {data.map((movie) => (
-            <MovieCard movie={movie} />
-          ))}
+class App extends React.Component {
+  componentDidMount() {
+
+  }
+  render() {
+    const  movies  = this.props.store.getState();
+
+    return (
+      <div className="App">
+        <Navbar />
+        <div className="main">
+          <div className="tabs">
+            <div className="tab">Movies</div>
+            <div className="tab">Favourites</div>
+          </div>
+          <div className="list">
+            {movies.map((movie, index) => (
+              <MovieCard movie={movie} key={`movies-${index}`} />
+            ))}
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
